f7e7735ce535b9b976def2cc211df6c7131ce99e
Sinegami <harrymgtu@gmail.com>
2019-06-19 11:45:58 +0300

    Add files via upload

---
+++ Hello_world/3_HelloWorld/3_HelloWorld.cpp
@@ -0,0 +1,20 @@
+using namespace std;
+#include<iostream>
+#include<fstream>
+#include<sstream>
+#include<cmath>
+#include<cstdlib>
+#include<string>
+#include<list>
+#include<vector>
+#include<algorithm>
+#include<stdexcept>
+inline void keep_window_open() { char ch; cin >> ch; }
+
+int main()
+{
+	cout << "Hello, programming!" << "\n";
+	cout << "Here we go!" << "\n";
+	keep_window_open();
+	return 0;
+}
