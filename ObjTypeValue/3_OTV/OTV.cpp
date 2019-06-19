bd8cc89d69f7254022d3d76d39a19578d6228eea
Sinegami <harrymgtu@gmail.com>
2019-06-19 15:18:18 +0300

    Add files via upload

---
+++ ObjTypeValue/3_OTV/OTV.cpp
@@ -0,0 +1,23 @@
+using namespace std;
+#include<iostream>
+#include<fstream>
+#include<sstream>
+#include<cmath>
+#include<cstdlib>
+#include<string>
+#include<list>
+#include<vector>
+#include<algorithm>
+#include<stdexcept>
+inline void keep_window_open() { char ch; cin >> ch; }
+
+int main()
+{
+	int age = -1;
+	string first_name = "???";
+	cout << "Input your name and age and press 'Enter'" << "\n";
+	cin >> first_name >> age;
+	cout << "Hello, " << first_name << " (age: " << age << ")!\n";
+	keep_window_open();
+	return 0;
+}
