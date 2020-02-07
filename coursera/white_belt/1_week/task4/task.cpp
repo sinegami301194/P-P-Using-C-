6953a98650ad6afb5d27651db04c67e65e014478
Sinegami <harrymgtu@gmail.com>
2020-02-07 15:00:07 +0300

    Create task.cpp

---
+++ coursera/white_belt/1_week/task4/task.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+	string a, b, c;
+	cin >> a >> b >> c;
+	if (a < b) {
+		if (a < c) {
+			cout << a << endl;
+		}
+		else
+			cout << c << endl;
+	}
+	else {
+		if (b < c) {
+			cout << b << endl;
+		}
+		else
+			cout << c << endl;
+	}
+	return 0;
+}
