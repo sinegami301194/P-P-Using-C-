080b2a575157f13394da1f67fbabf7a315e12395
Sinegami <harrymgtu@gmail.com>
2020-02-10 14:54:21 +0300

    Add files via upload

---
+++ coursera/white_belt/2_week/task3/task.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+struct Person {
+	string name;
+	string surname;
+	int age;
+};
+
+vector <Person> GetMoscowPopulation();
+
+void PrintPopulationSize(vector <Person> p) {
+	cout << "There are " << p.size() << 
+	" people in Moscow" << endl;
+}
+
+int main() {
+	vector <Person> people = GetMoscowPopulation();
+	PrintPopulationSize(people);
+	return 0;
+}
